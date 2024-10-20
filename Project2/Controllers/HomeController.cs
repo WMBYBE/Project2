6d54144e309e445bbfe91d4675625753a999347b
WMBYBE <williambbrouillette@gmail.com>
2024-10-20 18:20:07 -0500

    Can now add, edit, and delete data

--- Project2/Controllers/HomeController.cs
+++ Project2/Controllers/HomeController.cs
@@ -18,5 +18,49 @@ namespace Project2.Controllers
             var games = ctx.Games.OrderBy(g => g.Name).ToList();
             return View(games);
         }
+        public IActionResult Add()
+        {
+            return View("GameForm");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var g = ctx.Games.Find(Id);
+            return View("GameForm", g);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Game g)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (g.Id == 0)
+                {
+                    ctx.Games.Add(g);
+                }
+                else
+                {
+                    ctx.Games.Update(g);
+                }
+                ctx.SaveChanges();
+                var games = ctx.Games.ToList();
+                return View("Index", games);
+            }
+            else
+            {
+                return View("CharacterForm", g);
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+
+            Game del = ctx.Games.Find(id);
+            ctx.Games.Remove(del);
+            ctx.SaveChanges();
+            var g = ctx.Games.ToList();
+            return View("Index", g);
+        }
     }
 }
